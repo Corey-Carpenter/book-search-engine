5b6c2d41a552221e50df1001852e49772c946e6e
Corey Carpenter <stephcorey7@gmail.com>
2023-08-31 08:40:18 -0700

    got rid of wrong page, committing to push to heroku because localhost is being funky

--- client/src/App.js
+++ client/src/App.js
@@ -47,7 +47,6 @@ function App() {
           <Navbar />
             <Route exact path='/' component={SearchBooks} />
             <Route exact path='/saved' component={SavedBooks} />
-            <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
         </>
       </Router>
     </ApolloProvider>
